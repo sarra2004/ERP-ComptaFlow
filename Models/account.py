1508c68872e9fd9be1422b5ed8f968f1abd918b5
sarra2004 <172380849+sarra2004@users.noreply.github.com>
2025-12-04 16:51:46 +0100

    Add CompteComptable model to account.py

---
+++ Models/account.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from . import db
+
+class CompteComptable(db.Model):
+    __tablename__ = "compte_comptable"
+
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.String(20), unique=True, nullable=False)
+    intitule = db.Column(db.String(255), nullable=False)
+    classe = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(50))
+    status = db.Column(db.Enum('ACTIVE','INACTIVE', name='status_enum'), default='ACTIVE')
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
