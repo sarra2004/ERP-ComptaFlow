1c6ae447ce77e5b5c6bd9a92c13fedaf8dfec2b1
sarra2004 <172380849+sarra2004@users.noreply.github.com>
2025-12-04 17:01:58 +0100

    Set up Flask application with SQLAlchemy
    Initialize Flask app and configure SQLAlchemy.

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from config import db, Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialiser SQLAlchemy
+    db.init_app(app)
+
+    # Importer les modèles pour qu’ils soient reconnus par SQLAlchemy
+    with app.app_context():
+        from models.account import CompteComptable
+        from models.ecriture_models import Ecriture, JournalComptable, LigneEcriture
+        
+        db.create_all()  # créer les tables si elles n'existent pas
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
